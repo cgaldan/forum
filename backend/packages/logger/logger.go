c54f4e6eab13e07ad6da45188ded27d4c7938a24
cgaldan <chrisgaldan@gmail.com>
2026-02-26 00:13:32 +0200

    chore: rename pkg dir to packages for observability

--- backend/packages/logger/logger.go
+++ backend/packages/logger/logger.go
@@ -10,23 +10,16 @@ import (
 	"time"
 )
 
-// Level represents log level
 type Level int
 
 const (
-	// DebugLevel for debug messages
 	DebugLevel Level = iota
-	// InfoLevel for informational messages
 	InfoLevel
-	// WarnLevel for warning messages
 	WarnLevel
-	// ErrorLevel for error messages
 	ErrorLevel
-	// FatalLevel for fatal messages
 	FatalLevel
 )
 
-// String returns string representation of log level
 func (l Level) String() string {
 	switch l {
 	case DebugLevel:
@@ -44,13 +37,11 @@ func (l Level) String() string {
 	}
 }
 
-// Logger represents a logger
 type Logger struct {
 	logger *log.Logger
 	level  Level
 }
 
-// NewLogger creates a new logger
 func NewLogger(output io.Writer, level Level) *Logger {
 	return &Logger{
 		logger: log.New(output, "", 0),
@@ -58,29 +49,25 @@ func NewLogger(output io.Writer, level Level) *Logger {
 	}
 }
 
-// SetLevel sets the logging level
 func (l *Logger) SetLevel(level Level) {
 	l.level = level
 }
 
-func (l *Logger) log(level Level, message string, args ...interface{}) {
+func (l *Logger) log(level Level, message string, args ...any) {
 	if level < l.level {
 		return
 	}
 
-	// Get caller information
 	_, file, line, ok := runtime.Caller(2)
 	caller := "???"
 	if ok {
-		// Extract just the filename
 		parts := strings.Split(file, "/")
 		caller = fmt.Sprintf("%s:%d", parts[len(parts)-1], line)
 	}
 
 	timestamp := time.Now().Format("2006-01-02 15:04:05")
-	prefix := fmt.Sprintf("[%s] %s %s", timestamp, level.String(), caller)
+	prefix := fmt.Sprintf("[%s] %s %s ", timestamp, level.String(), caller)
 
-	// Format additional arguments
 	var argsStr string
 	if len(args) > 0 {
 		var pairs []string
@@ -94,32 +81,26 @@ func (l *Logger) log(level Level, message string, args ...interface{}) {
 		}
 	}
 
-	l.logger.Printf("%s - %s%s\n", prefix, message, argsStr)
+	l.logger.Printf("%s%s%s", prefix, message, argsStr)
 }
 
-// Debug logs a debug message
-func (l *Logger) Debug(message string, args ...interface{}) {
+func (l *Logger) Debug(message string, args ...any) {
 	l.log(DebugLevel, message, args...)
 }
 
-// Info logs an informational message
-func (l *Logger) Info(message string, args ...interface{}) {
+func (l *Logger) Info(message string, args ...any) {
 	l.log(InfoLevel, message, args...)
 }
 
-// Warn logs a warning message
-func (l *Logger) Warn(message string, args ...interface{}) {
+func (l *Logger) Warn(message string, args ...any) {
 	l.log(WarnLevel, message, args...)
 }
 
-// Error logs an error message
-func (l *Logger) Error(message string, args ...interface{}) {
+func (l *Logger) Error(message string, args ...any) {
 	l.log(ErrorLevel, message, args...)
 }
 
-// Fatal logs a fatal message and exits
-func (l *Logger) Fatal(message string, args ...interface{}) {
+func (l *Logger) Fatal(message string, args ...any) {
 	l.log(FatalLevel, message, args...)
 	os.Exit(1)
 }
-
